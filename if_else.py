0c3acf3dde550d3b0f16a84f2e4a47b3f52393fb
kamran <kamran.anwar091@gmail.com>
2026-02-02 18:20:42 +0500

    python code

---
+++ if_else.py
@@ -0,0 +1,35 @@
+# a = int(input   ("Enter first number: "))
+# b = int(input   ("Enter second number: "))
+
+# if a > b:
+#     print("A is greater")
+
+# elif a < b:
+#     print("B is greater")
+
+# else:
+#     print("Both are equal")
+
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+
+# if num1 >= num2 and num1 >= num3:
+#     print("Largest number is: ", num1)
+
+# elif num2 >= num1 and num2 >= num3:
+#     print("Largest number is: ", num2)
+
+# else:
+#     print("Largest number is: ", num3)
+
+# year = int(input("Enter year: "))
+
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("Leap year")
+# else:
+#     print("Not a leap year")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
