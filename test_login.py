d4e4221fb4a291bce0eb04e1818ab7b5fe141709
kamran <kamran.anwar091@gmail.com>
2026-02-02 12:33:39 +0500

    code fix

--- test_login.py
+++ test_login.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import expect
+
 from login_page import LoginPage
 
 def test_login():
@@ -18,9 +20,14 @@ def test_login():
         
         # Optional: Assert successful login
         # Example: check if dashboard is visible
-        user_label = page.locator("text='Muhammad Kamran Anwar'")
-        user_label.wait_for(timeout=10000)  # Wait for element
-        assert user_label.is_visible(), "Login failed!"
+        # expect(page.get_by_text("Muhammad Kamran Anwar")).to_be_visible()
+        hea_ding = page.locator("text=Muhammad Kamran Anwar")
+        expect(hea_ding).to_be_visible()
+        print("Text Found:", hea_ding.text_content())
+
+        # user_label = page.locator("text='Muhammad Kamran Anwar'")
+        # user_label.wait_for(timeout=10000)  # Wait for element
+        # assert user_label.is_visible(), "Login failed!"
         # assert page.is_visible("text=Muhammad Kamran Anwar"), "Login failed!"
         
         # Close browser
